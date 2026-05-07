b983d21b5a606658b63c221399fe3c06f479d33d
mdetoeuf <morgane.detoeuf@gmail.com>
2026-05-07 09:36:53 +0200

    improvements on readability of import-tidy vignette, and export of the tidy_table as a user-accessible data set (+ documentation)

---
+++ data-raw/tidy_table.R
@@ -0,0 +1,35 @@
+## code to prepare `tidy_table` dataset goes here
+
+usethis::use_data(tidy_table, overwrite = TRUE)
+
+skanit_csv <- system.file("extdata", "skanit.csv", package = "plate2N")
+
+skanit <- skanit_to_tibble(skanit_csv)
+
+joined_vertical <- join_abs_map(
+  skanit$abs_tibble,
+  skanit$map_tibble,
+  dataset = "expe1-")
+
+too_long <- joined_vertical |>
+  tidyr::pivot_longer(
+    cols = !any_of(c("row", "column")),
+    names_to = c("dataset", "abs_map", "plate_id"),
+    names_sep = "-",
+    values_to = "value"
+  )
+
+tidy_table <- too_long |>
+    tidyr::pivot_wider(
+      names_from = "abs_map",
+      values_from = "value"
+    ) |>
+    dplyr::relocate(map, .before = "abs" ) |>
+    dplyr::mutate(
+      well_id = paste0(row, column),
+      unique_well_id = paste0(well_id, "_", plate_id),
+      .before = 3
+    ) |>
+    dplyr::relocate(plate_id, .before = "unique_well_id")
+
+#usethis::use_data(tidy_table, overwrite = TRUE)
